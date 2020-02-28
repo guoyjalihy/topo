d3bcbbb853958dbd6cb4309319ee75a74bfa8382
guoyjalihy <guoyjalihy@icloud.com>
2020-02-28 16:29:04 +0800

    增加供电系统

--- js/data.js
+++ js/data.js
@@ -1,5 +1,6 @@
 // 定义机柜、服务器数据
 var model = [
+    //1
     {
         cabinet:
             {
@@ -428,6 +429,7 @@ var model = [
             }
     },
 
+    //2
     {
         cabinet:
             {
@@ -456,13 +458,13 @@ var model = [
                 'size': {w: 30, h: 70, d: 30}, // 尺寸 宽、高、深
                 'position': {x: -440, y: 0, z: 320}, // 位置
                 'servers': [
-                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': ''}
+                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': '','cpuAlloc':90},
+                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': '','cpuAlloc':80},
+                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': '','cpuAlloc':70},
+                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': '','cpuAlloc':60},
+                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': '','cpuAlloc':50},
+                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': '','cpuAlloc':40},
+                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': '','cpuAlloc':30}
                 ]
             }
     },
@@ -856,6 +858,7 @@ var model = [
             }
     },
 
+    //3
     {
         cabinet:
             {
@@ -1284,7 +1287,7 @@ var model = [
             }
     },
 
-
+    //4
     {
         cabinet:
             {
@@ -1713,6 +1716,7 @@ var model = [
             }
     },
 
+    //5
     {
         cabinet:
             {
@@ -2141,7 +2145,7 @@ var model = [
             }
     },
 
-
+    //6
     {
         cabinet:
             {
@@ -2298,13 +2302,13 @@ var model = [
                 'size': {w: 30, h: 70, d: 30}, // 尺寸 宽、高、深
                 'position': {x: -200, y: 0, z: 0}, // 位置
                 'servers': [
-                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':40},
-                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': '','cpuUsed': 64,'memUsed':25,'temperature':40},
-                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':65,'temperature':40},
-                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':60},
-                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':65,'temperature':40},
-                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': '','cpuUsed': 64,'memUsed':25,'temperature':40},
-                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':60}
+                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':40,'cpuAlloc':60,'memAlloc':60},
+                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': '','cpuUsed': 64,'memUsed':25,'temperature':40,'cpuAlloc':40,'memAlloc':60},
+                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':65,'temperature':40,'cpuAlloc':30,'memAlloc':20},
+                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':60,'cpuAlloc':90,'memAlloc':30},
+                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':65,'temperature':40,'cpuAlloc':70,'memAlloc':50},
+                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': '','cpuUsed': 64,'memUsed':25,'temperature':40,'cpuAlloc':10,'memAlloc':10},
+                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': '','cpuUsed': 34,'memUsed':25,'temperature':60,'cpuAlloc':80,'memAlloc':70}
                 ]
             }
     },
@@ -2834,13 +2838,13 @@ var model = [
                 'size': {w: 30, h: 70, d: 30}, // 尺寸 宽、高、深
                 'position': {x: 70, y: 0, z: -80}, // 位置
                 'servers': [
-                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': ''},
-                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': ''}
+                    {'uuid': '11', 'id':11, 'name':'1号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':60,'memUsed':60,'cpus':60},
+                    {'uuid': '12', 'id':12, 'name':'2号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':70,'memUsed':60,'cpus':20},
+                    {'uuid': '13', 'id':13, 'name':'3号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':30,'memUsed':20,'cpus':60},
+                    {'uuid': '14', 'id':14, 'name':'4号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':10,'memUsed':60,'cpus':90},
+                    {'uuid': '15', 'id':15, 'name':'5号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':80,'memUsed':30,'cpus':60},
+                    {'uuid': '16', 'id':16, 'name':'6号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':20,'memUsed':90,'cpus':40},
+                    {'uuid': '17', 'id':17, 'name':'7号服务器', 'ip':'', 'apps': '','cpuAlloc':60,'memAlloc':10,'memUsed':60,'cpus':50}
                 ]
             }
     },
